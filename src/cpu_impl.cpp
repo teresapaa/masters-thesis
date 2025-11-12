f0835bd56f64c74e7483ff281d299699135153c3
Teresa Paatero <teresa.paatero@bof.fi>
2025-11-12 10:51:03 +0200

    First working version of Neoclassical CPU

--- src/cpu_impl.cpp
+++ src/cpu_impl.cpp
@@ -3,19 +3,33 @@
 #include <vector>
 using namespace std;
 
+bool is_binding(const vector<int>& g, float n_k) {
 
+    for ( int idx : g) {
+        if (idx == 1 || idx == n_k -1 ) return true; ;
+    }
+    return false;
+}
+
+float max_abs_difference(const std::vector<float>& V0, const std::vector<float>& V1) {
+    float d = 0.0;
+    for (size_t i = 0; i < V0.size(); ++i) {
+        float diff = std::abs(V0[i] - V1[i]);
+        if (diff > d) d = diff;
+    }
+    return d;
+}
 
 extern "C" void run_compute() {
-    cout << "Neoclassical Growth model [no GPU]\n";
+    cout << "Neoclassical Growth model [no GPU]" << endl;
 
     //Setting up variables
-    int n_k = 100; // number of grid points
+    int n_k = 1000; // number of grid points
     float Kmin = 0.5f; // lower bound of the state space
-    float Kmax = 4.5f; // upper bound of the state space
+    float Kmax = 100.0f; // upper bound of the state space
     float epsilon = 0.001f; //tolerance of error
     float alpha = 0.5f; //capital share
-    int z = 1; //productivity
-    float delta = 0.03f; //fix this to be something meaningful
+    float z = 1.0f; //productivity
     float beta = 0.96f; //annual discounting
     float delta = 0.025f; //annual depreciation
 
@@ -30,6 +44,7 @@ extern "C" void run_compute() {
         //K_j = capital-option of the current period
         //V_j = the previous value function at the point J 
     auto V = [z, delta, beta, F, u](float K_i, float K_j, float V_j) { return u(z * F(K_i) + (1 - delta) * K_i - K_j) + beta * V_j;};
+    
     auto C = [delta, z, F](float K_i, float K_j) {return z * F(K_i) + (1 - delta) * K_i - K_j;};
     
     //Set the grid points
@@ -41,30 +56,56 @@ extern "C" void run_compute() {
     vector<float> V_old(n_k, 0.0);
     vector<float> V_new(n_k, 0.0);
     vector<float> V_conditionals(n_k, 0.0);
-    vector<int> g;
+    vector<int> g(n_k, 0);
 
-    double diff;
+    const float NEG_INF = -1e300;
+    float diff =0.0f;
     int iteration = 0;
+    const int max_iter = 20000;
 
     //Value function iteration loop
     do {
         for (int i = 0; i < n_k; i++) {
             for (int j = 0; j < n_k; j++) {
-                if (C(K[i], K[j]) < 0) { V_conditionals[j] = 100000000.0f; }
+                if (C(K[i], K[j]) < 0) { V_conditionals[j] = NEG_INF; }
                 else { V_conditionals[j] = V(K[i], K[j], V_old[j]); }
 
             }
-            float max = *max_element(V_conditionals.begin(), V_conditionals.end());
-            // int index = std::distance(V_conditionals.begin(), max);  <- what is the problem?
-            // g.push_back(index);
-            V_new[i] = max;
+            auto max_elem = max_element(V_conditionals.begin(), V_conditionals.end());
+            int index = std::distance(V_conditionals.begin(), max_elem);
+            g[i] = index;
+            V_new[i] = *max_elem;
         }
+    
+        diff = max_abs_difference(V_old, V_new);
 
-        //diff = ...
+        V_old = V_new;
+        ++iteration;
 
-    } while (diff < epsilon);
-     
-    //Check the results
+    } while (diff > epsilon && iteration < max_iter);
 
-        
+    cout << "Found a solution after " << iteration << " iterations" << endl;
+    cout << "Final diff: " << diff << endl;
+
+    //Are the bounds of the state space binding?
+    if (is_binding(g, n_k)) {
+        cout << "State space is binding" << endl;
+    }
+
+    //Print out the optimal policy:
+    std::cout << "Optimal policy g:" << std::endl;
+    for (int x : g) {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+       
+    cout << "Corresponding K' values for each K:" << endl;
+
+    for (int x : g) {
+        std::cout << K[x] << " ";
+    }
+    std::cout << std::endl;
+
 }
+
+
